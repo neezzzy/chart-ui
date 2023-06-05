382661d1a52014cc68bbd51daa98d702cdd2c8f4
Russell Nizamov <nizamov.ruslan@gmail.com>
2023-06-04 22:35:01 -0700

    add input component

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,90 @@
-import React, { useCallback } from 'react';
-import ReactFlow, { useNodesState, useEdgesState, addEdge } from 'reactflow';
+import React, { useEffect, useState } from "react";
+import ReactFlow, { useNodesState, useEdgesState } from "reactflow";
+import "reactflow/dist/style.css";
 
-import 'reactflow/dist/style.css';
+import "./style.css";
 
 const initialNodes = [
-  { id: '1', position: { x: 0, y: 0 }, data: { label: '1' } },
-  { id: '2', position: { x: 0, y: 100 }, data: { label: '2' } },
+  { id: "1", data: { label: "-" }, position: { x: 100, y: 100 } },
+  { id: "2", data: { label: "Node 2" }, position: { x: 100, y: 200 } },
 ];
-const initialEdges = [{ id: 'e1-2', source: '1', target: '2' }];
 
-export default function App() {
+const initialEdges = [{ id: "e1-2", source: "1", target: "2" }];
+const defaultViewport = { x: 0, y: 0, zoom: 1 };
+
+const UpdateNode = () => {
   const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
   const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
 
-  const onConnect = useCallback((params) => setEdges((eds) => addEdge(params, eds)), [setEdges]);
+  const [nodeName, setNodeName] = useState("type your prompt here");
+  useEffect(() => {
+    setNodes((nds) =>
+      nds.map((node) => {
+        if (node.id === "1") {
+          // it's important that you create a new object here
+          // in order to notify react flow about the change
+          node.data = {
+            ...node.data,
+            label: nodeName,
+          };
+        }
+        if (node.id === "2") {
+          // it's important that you create a new object here
+          // in order to notify react flow about the change
+          node.data = {
+            ...node.data,
+            label: nodeName,
+          };
+        }
+
+        return node;
+      })
+    );
+  }, [nodeName, setNodes]);
+
+  useEffect(() => {
+    setNodes((nds) =>
+      nds.map((node) => {
+        if (node.id === "1") {
+          // when you update a simple type you can just update the value
+        }
+
+        return node;
+      })
+    );
+    setEdges((eds) =>
+      eds.map((edge) => {
+        if (edge.id === "e1-2") {
+          return edge;
+        }
+
+        return edge;
+      })
+    );
+  }, [setNodes, setEdges]);
 
   return (
-    <div style={{ width: '100vw', height: '100vh' }}>
+    <div style={{ width: 500, height: 500 }}>
       <ReactFlow
         nodes={nodes}
         edges={edges}
         onNodesChange={onNodesChange}
         onEdgesChange={onEdgesChange}
-        onConnect={onConnect}
-      />
+        defaultViewport={defaultViewport}
+        minZoom={0.2}
+        maxZoom={4}
+        attributionPosition="bottom-left"
+      >
+        <div className="updatenode__controls">
+          <label>label:</label>
+          <input
+            value={nodeName}
+            onChange={(evt) => setNodeName(evt.target.value)}
+          />
+        </div>
+      </ReactFlow>
     </div>
   );
-}
+};
+
+export default UpdateNode;

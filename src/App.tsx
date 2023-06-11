05698f5ea452b2f089daef382e30542363cf6808
Russell Nizamov <nizamov.ruslan@gmail.com>
2023-06-11 00:20:42 -0700

    add deleteAll createNode functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,90 +1,134 @@
-import React, { useEffect, useState } from "react";
-import ReactFlow, { useNodesState, useEdgesState } from "reactflow";
+import React, { useCallback, useEffect, useRef, useState } from "react";
+import ReactFlow, {
+  useNodesState,
+  useEdgesState,
+  Controls,
+  getIncomers,
+  getOutgoers,
+  getConnectedEdges,
+  ControlButton,
+} from "reactflow";
 import "reactflow/dist/style.css";
 
-
+import "./App.css";
 
 const initialNodes = [
-  { id: "1", data: { label: "-" }, position: { x: 100, y: 100 } },
-  { id: "2", data: { label: "Node 2" }, position: { x: 100, y: 200 } },
+  {
+    id: "0",
+    type: "input",
+    data: { label: "Node" },
+    position: { x: 0, y: 50 },
+  },
 ];
 
-const initialEdges = [{ id: "e1-2", source: "1", target: "2" }];
-const defaultViewport = { x: 0, y: 0, zoom: 1 };
-
-const UpdateNode = () => {
+const App = () => {
+  const reactFlowWrapper = useRef(null);
   const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
-  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+  const [edges, setEdges, onEdgesChange] = useEdgesState([]);
+  const [prevNodePosition, setPrevNodePosition] = useState({ x: 0, y: 50 });
 
-  const [nodeName, setNodeName] = useState("type your prompt here");
+  const generateUniqueId = () => {
+    const timestamp = Date.now();
+    return `node-${timestamp}`;
+  };
+
+  const handleAddNode = useCallback(() => {
+    if (prevNodePosition === null) {
+      setPrevNodePosition({ x: 0, y: 50 });
+    } else {
+      const { x, y } = prevNodePosition;
+      const id = generateUniqueId();
+
+      const newNode = {
+        id,
+        position: { x, y: y + 50 },
+        data: { label: `Node ${id}` },
+      };
+
+      setPrevNodePosition({ x, y: y + 50 });
+      setNodes((nds) => nds.concat(newNode));
+    }
+  }, [prevNodePosition, setNodes]);
+
+  const handleDeleteAllNodes = () => {
+    setNodes([]);
+    setEdges([]);
+    setPrevNodePosition({ x: 0, y: 50 });
+  };
+
+  const onNodesDelete = useCallback(
+    (deleted) => {
+      setEdges(
+        deleted.reduce((acc, node) => {
+          const incomers = getIncomers(node, nodes, edges);
+          const outgoers = getOutgoers(node, nodes, edges);
+          const connectedEdges = getConnectedEdges([node], edges);
+
+          const remainingEdges = acc.filter(
+            (edge) => !connectedEdges.includes(edge)
+          );
+
+          const createdEdges = incomers.flatMap(({ id: source }) =>
+            outgoers.map(({ id: target }) => ({
+              id: `${source}->${target}`,
+              source,
+              target,
+            }))
+          );
+
+          return [...remainingEdges, ...createdEdges];
+        }, edges)
+      );
+    },
+    [setEdges, edges, nodes]
+  );
+
   useEffect(() => {
-    setNodes((nds) =>
-      nds.map((node) => {
-        if (node.id === "1") {
-          // it's important that you create a new object here
-          // in order to notify react flow about the change
-          node.data = {
-            ...node.data,
-            label: nodeName,
-          };
-        }
-        if (node.id === "2") {
-          // it's important that you create a new object here
-          // in order to notify react flow about the change
-          node.data = {
-            ...node.data,
-            label: nodeName,
-          };
-        }
+    const handleKeyDown = (event) => {
+      if (event.key === "n" || event.key === "N") {
+        handleAddNode();
+      }
+    };
 
-        return node;
-      })
-    );
-  }, [nodeName, setNodes]);
-
-  useEffect(() => {
-    setNodes((nds) =>
-      nds.map((node) => {
-        if (node.id === "1") {
-          // when you update a simple type you can just update the value
-        }
-
-        return node;
-      })
-    );
-    setEdges((eds) =>
-      eds.map((edge) => {
-        if (edge.id === "e1-2") {
-          return edge;
-        }
-
-        return edge;
-      })
-    );
-  }, [setNodes, setEdges]);
+    document.addEventListener("keydown", handleKeyDown);
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [handleAddNode]);
 
   return (
-    <div style={{ width: 500, height: 500 }}>
+    <div
+      className="wrapper"
+      ref={reactFlowWrapper}
+      style={{ width: "100%", height: "100vh" }}
+    >
       <ReactFlow
         nodes={nodes}
         edges={edges}
         onNodesChange={onNodesChange}
+        onNodesDelete={onNodesDelete}
         onEdgesChange={onEdgesChange}
-        defaultViewport={defaultViewport}
-        minZoom={0.2}
-        maxZoom={4}
-        attributionPosition="bottom-left"
+        fitView
       >
-        <div className="updatenode__controls">
-          <label>label:</label>
-          <input
-            value={nodeName}
-            onChange={(evt) => setNodeName(evt.target.value)}
-          />
-        </div>
+        <Controls showZoom={false} showFitView={false} showInteractive={false}>
+          <ControlButton
+            className="button"
+            onClick={handleAddNode}
+            title="action"
+          >
+            <div>press "N" or add node</div>
+          </ControlButton>
+          <ControlButton
+            className="button"
+            onClick={handleDeleteAllNodes}
+            title="action"
+          >
+            <div>delete all nodes</div>
+          </ControlButton>
+        </Controls>
       </ReactFlow>
     </div>
   );
 };
 
-export default UpdateNode;
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import { ReactFlowProvider } from "reactflow";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ReactFlowProvider>
+      <App />
+    </ReactFlowProvider>
+  </React.StrictMode>
+);
